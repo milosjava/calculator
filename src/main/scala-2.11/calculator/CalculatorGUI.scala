8e91331f97563b8e4243727aabdb13079a660c2e
Milos Grubjesic <milosjava@gmail.com>
2015-11-03 00:32:12 +0100

    add buttons , small fixes for calculator layout

--- src/main/scala-2.11/calculator/CalculatorGUI.scala
+++ src/main/scala-2.11/calculator/CalculatorGUI.scala
@@ -9,14 +9,20 @@ import javafx.scene.image.Image
 import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.scene.Scene
-import scalafx.scene.control.TextField
+import scalafx.scene.control.{Button, TextField}
+import scalafx.scene.layout.{BorderPane, GridPane}
 import scalafx.scene.paint.Color
 import scalafx.scene.shape.Rectangle
+import scalafx.scene.text.Font
 
 object CalculatorGUI extends JFXApp {
 
   stage = new JFXApp.PrimaryStage {
     title.value = "TopTal - Calculator"
+
+    //really no need to resize it I guess
+    resizable = false
+
     //todo see how to set icon
     //icons += new Image("calculator/calculator-icon.png")
 
@@ -26,7 +32,11 @@ object CalculatorGUI extends JFXApp {
     height = 250
     centerOnScreen()
     scene = new Scene  {
-      fill = Color.rgb(242,241,240)
+
+
+      fill = Color.rgb(242,241,240) //RGB numbers same as calculator in ubuntu
+
+
       val text = new TextField
 
       text.prefHeight = 60
@@ -35,10 +45,55 @@ object CalculatorGUI extends JFXApp {
       text.layoutX = 5
       text.layoutY = 5
 
+      //set inout font  and size
+      text.setFont(Font.font("Serif", 22))
 
-      //todo limit text in the input field
+      //now add pane
+      val pane = new GridPane
 
-      content = List(text)
+      pane.setHgap(10)
+      pane.setVgap(10)
+
+      //define buttons - numbers
+      val button1 = new Button("1")
+      val button2 = new Button("2")
+      val button3 = new Button("3")
+      val button4 = new Button("4")
+      val button5 = new Button("5")
+      val button6 = new Button("6")
+      val button7 = new Button("7")
+      val button8 = new Button("8")
+      val button9 = new Button("9")
+
+      val divide  = new Button("/")
+      val plus  = new Button("+")
+      val minus  = new Button("-")
+      val floatPoint  = new Button(".")
+      val startGroup  = new Button("(")
+      val endGroup  = new Button(")")
+      val equals  = new Button("=")
+      val X  = new Button("X")
+      val multiply = new Button("*")
+      val delete = new Button("C")
+      val log = new Button("log")
+
+      val calculate = new Button("CALC")
+
+
+      //define buttons - operations
+
+
+
+      //add buttons to pane and position them in the grid
+      pane.add(button1,1,0)
+      pane.add(button2,2,0)
+      pane.add(button3,3,0)
+      pane.add(button4,1,1)
+
+      pane.setLayoutY(80)
+
+      //create content
+      content = List(text,pane)
 
     }
 
